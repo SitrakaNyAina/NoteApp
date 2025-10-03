ea5363449be12efed938bf5ba97525db68f04c07
SitrakaNyAina <sitrakanyainanomenjanahary@gmail.com>
2025-10-03 21:38:43 +0300

    modified:   frontend/vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    port: 3000
+  },
+  // 👇 Ajoute ce bloc pour Vercel
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  base: './',
+});
